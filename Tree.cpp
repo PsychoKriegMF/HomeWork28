9a909f31f4f132ea512bc8017a1a024902d2840f
Mikhail <Koltsov.itmh@gmail.com>
2024-01-14 17:15:48 +0500

    Добавьте файлы проекта.

---
+++ Tree.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include "Tree.hpp"
+
+void print_tree(const Tree& T) {
+	std::cout << "Название плода: " << T.fruit << std::endl;
+	std::cout << "Кол-во плодов на дереве: " << T.fruits_number << std::endl;
+	std::cout << "Цена за один плод: " << T.fruits << std::endl;
+}
+
+int full_price(const Tree& T) {
+	return T.fruits * T.fruits_number;
+}
+int collect(const Tree& T, int num) {
+	if (num > T.fruits_number || num <= 0)
+		return 1;
+	return T.fruits * num;   // как я понял продаем именно сорванные плоды, то кол-во, на которое уменьшилось 
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "Tree.hpp"
+
+
+
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	Tree kiwi{
+		"Киви",
+		10,
+		5
+	};
+
+	print_tree(kiwi);
+	std::cout << std::endl;
+
+	std::cout << "Стоимость всех плодов на дереве = " << full_price(kiwi) << std::endl;
+
+	int n;
+	std::cout << "Введите кол-во сорваных фруктов -> ";
+	std::cin >> n;
+	if (collect(kiwi,n) == 1)
+		std::cout << "Ошибка ввода!" << std::endl;
+	else
+		std::cout << "Сорванные плоды можно продать за " << collect(kiwi,n) << std::endl;
+
+
+
+	return 0;
+}
